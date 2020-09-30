7c414e376ae5d5545d4de6cad69d48ac50e07842
Exd226 <exd226@gmail.com>
2020-10-01 00:19:32 +0900

    add route

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello Flask"
+def index():
+    return render_template('index.html')
+
+@app.route('/room')
+def create_room():
+    return render_template('room.html')
+
+@app.route('/room/category')
+def select_category():
+    return render_template('category.html')
+
+@app.route('/room/category/result')
+def result():
+    return render_template('result.html')
+
+@app.route('/user')
+def my_page():
+    return render_template('user.html')
 
 if __name__ == '__main__':
     app.run()
