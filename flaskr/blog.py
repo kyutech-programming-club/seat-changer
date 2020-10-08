02cd00722f6a0b944c2cacb32b2707e12fbf525d
take-cantik <miki.takeru508@mail.kyutech.jp>
2020-10-09 00:04:13 +0900

    Delete print

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -25,7 +25,6 @@ def get_post(id, check_author=True):
 @bp.route('/')
 def index():
   db = get_db()
-  print(db)
   posts = db.execute(
     'SELECT p.id, title, body, created, author_id, username'
     ' FROM post p JOIN user u ON p.author_id = u.id'
