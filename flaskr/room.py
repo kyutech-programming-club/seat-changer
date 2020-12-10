b7965da967102cd2101e6786295c7985ccf329f8
take-cantik <miki.takeru508@mail.kyutech.jp>
2020-12-10 19:56:25 +0900

    Delete import at room.py

--- flaskr/room.py
+++ flaskr/room.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
-import itertools
-import random
 from . import seat
 
 from flaskr.auth import login_required
